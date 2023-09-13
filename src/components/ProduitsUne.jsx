22e72c03f6ba62c4a525760b0dc32b8728173ddd
wassim-mouloud <simo.rko10@gmail.com>
2023-09-13 22:44:48 +0200

    slider cards for mobile

--- src/components/ProduitsUne.jsx
+++ src/components/ProduitsUne.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProductCard from './ProductCard'
 
 function ProduitsUne() {
+
+    const [slider, setSlider] = useState([0,1,2,3,4,5,6,7])
+
   return (
-    <div>
-        <div className='flex gap-4' >
-            <ProductCard/>
+    <div className='' >
+        <h2 className='my-6 font-bold text-center text-black text-[24px]' >Produits à la une</h2>
+        <div className='' >
+            <div className='flex items-center h-[390px] gap-4 overflow-x-scroll p-4' >
+                {slider.map((card =>{
+                    return (
+                        <ProductCard img='med' name='Pharmasin' price='2500' />
+                    )
+                }))}
+            </div>
         </div>
     </div>
   )
--- src/sections/MainPage.jsx
+++ src/sections/MainPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import ProduitsUne from '../components/ProduitsUne'
 
 function MainPage({openNav, setOpenNav}) {
   return (
     <div>
         <Navbar openNav={openNav} setOpenNav={setOpenNav} />
         <Hero/>
+        <ProduitsUne/>
     </div>
   )
 }
