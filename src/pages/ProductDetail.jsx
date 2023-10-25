2f94bd8e0c8ca85c5c59e0f7d38f1d0254a72aa7
wassim-mouloud <simo.rko10@gmail.com>
2023-10-25 18:47:57 +0200

    bug fix

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -4,6 +4,7 @@ import { doc, getDoc} from "firebase/firestore";
 import {db} from '../config/firebase'
 import Navbar from '../components/Navbar';
 import Contact from '../components/Contact';
+
 function ProductDetail({openNav, setOpenNav}) {
 
     const [product, setProduct] = useState({})
@@ -27,7 +28,7 @@ function ProductDetail({openNav, setOpenNav}) {
     <div>
         <Navbar openNav={openNav} setOpenNav={setOpenNav} />
         <div className='flex flex-col items-center justify-center gap-6 ' >
-            <img src={product.imgUrl} alt="" className='w-[90vw] md:w-[400px] lg:w-[400px] xl:w-[500px] object-contain' />
+            <img src={product.imgUrl} alt="" className='w-[80vw] sm:w-[300px] md:w-[400px] lg:w-[400px] xl:w-[420px] object-contain' />
             <div className='flex flex-col gap-3 p-4' >
                 <div className='flex items-center gap-3' >
                     <span className='bg-[#17a841] w-[5px] h-[40px] ' ></span>
